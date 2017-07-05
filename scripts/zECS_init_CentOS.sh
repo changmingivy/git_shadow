1cdf58ec2f1af9486835546ddf7f5d19b244d784
fh </tmp/aetc>
2017-07-05 18:42:28 +0800

    修正ECS初始化脚本在CentOS6环境下不能运行的问题

--- scripts/zECS_init_CentOS.sh
+++ scripts/zECS_init_CentOS.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-
 zCodePath=~git/miaopai
 zEcsAddrListPath=$zCodePath/.git_shadow/info/client_ip_all.txt #store all ECSs' private IPs
 zEcsAddrMajorListPath=$zCodePath/.git_shadow/info/client_ip_major.txt #store all major ECSs' public IPs
@@ -21,31 +20,31 @@ cd $zCodePath
 git config --global user.email "ECS@aliyun.com"
 git config --global user.name "ECS"
 git add --all .
-git commit -m "init"
+git commit -m "INIT"
 git branch -m master client # 将master分支名称更改为client
 git branch server # 创建server分支
 
 # config git hook
 # 拉取server分支分代码到client分支；通知中控机已收到代码；判断自身是否是ECS分发节点，如果是，则向同一项目下的所有其它ECS推送最新收到的代码
-printf "#!/bin/sh \n\
-	git pull --force $zCodePath/.git server:client \n\
-	$zCodePath/.git_shadow/bin/git_shadow -h 10.30.2.126 -p 20000 -C \n\
+printf "\
+#!/bin/sh \n\
+git pull --force $zCodePath/.git server:client \n\
+$zCodePath/.git_shadow/bin/git_shadow -C -h 10.30.2.126 -p 20000 \n\
 
-	cp -up $zCodePath/.git_shadow/authorized_keys ~git/.ssh/ \n\
+cp -up $zCodePath/.git_shadow/authorized_keys ~git/.ssh/ \n\
 
-	zMyAddr=`ip addr | grep -oP '(\d+.){3}\d+(?=/\d+)' | grep -v '^127.0.0'` \n\
-	zMajorAddrList=`cat $zEcsAddrMajorListPath` \n\
+for zAddr in \$(ip addr | grep -oP \'(\\d+\\.){3}\\d+(?=/\\d+)\' | grep -v \'^127.0.0\') \n\
+do \n\
+    if [[ 0 -lt \$(cat $zEcsAddrMajorListPath | grep -c \$zAddr) ]]; then \n\
+        zEcsAddrList=\$(cat $zEcsAddrListPath | tr \'\\\n\' \' \') \n\
+        for zEcsAddr in \$zEcsAddrList \n\
+        do \n\
+            git push --force git@\${zEcsAddr}:${zCodePath}/.git server:client &\n\
+        done \n\
+        break \n\
+    fi \n\
+done \n\
+" > $zCodePath/.git/hooks/post-receive
 
-	for zAddr in $zMyAddr \n\
-	do \n\
-		if [[ 0 -lt `echo $zMajorAddrList | grep -c $zAddr` ]]; then \n\
-			zEcsAddrList=`cat $zEcsAddrListPath` \n\
-			for zEcsAddr in $zEcsAddrList \n\
-			do \n\
-				git push --force git@${zEcsAddr}:${zCodePath}/.git server:client \n\
-			done \n\
-			break \n\
-		fi \n\
-	done" > $zCodePath/hooks/post-receive
-
-chmod u+x $zCodePath/hooks/post-receive
+chmod u+x $zCodePath/.git/hooks/post-receive
+chown -R git:git ~git
