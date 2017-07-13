6900c6bb154143c9338061e195f684b5bc722b36
git_shadow <git_shadow@yixia.com>
2017-07-13 16:19:49 +0800

    _

--- scripts/zECS_init_CentOS.sh
+++ scripts/zECS_init_CentOS.sh
@@ -40,7 +40,7 @@ git branch server # 创建server分支
 # config git hook
 # 拉取server分支分代码到client分支；通知中控机已收到代码；判断自身是否是ECS分发节点，如果是，则向同一项目下的所有其它ECS推送最新收到的代码
 printf "#!/bin/sh
-    cd $zCodePath  # 必须首先切换路径，否则 reset 不会执行
+    cd $zCodePath &&  # 必须首先切换路径，否则 reset 不会执行
 
     export PATH=\"/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin\" &&
     export HOME=\"/home/git\" &&
@@ -48,7 +48,7 @@ printf "#!/bin/sh
 
     git checkout server &&
     git checkout -b TMP &&
-    git reset -q --hard && # 注：代码初始状态只是接收到git库中，需要将其reset至工作区路径
+    git reset -q --hard &&  # 注：代码初始状态只是接收到git库中，需要将其reset至工作区路径
     git branch -M client &&
 
 (
