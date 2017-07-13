29423a84db4dcb4ef52f78bd25825d2c677ee312
git_shadow <git_shadow@yixia.com>
2017-07-13 20:30:32 +0800

    _

--- scripts/zECS_init_CentOS.sh
+++ scripts/zECS_init_CentOS.sh
@@ -36,7 +36,7 @@ git branch server # 创建server分支
 printf "#!/bin/sh
     export PATH=\"/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin\" &&
     export HOME=\"/home/git\" &&
-    export GIT_DIR=\"%zCodePath/.git\" # 设定git hook 工作路径，默认为'.'，即hook文件所在路径，会带来异常
+    export GIT_DIR=\"$zCodePath/.git\" # 设定git hook 工作路径，默认为'.'，即hook文件所在路径，会带来异常
     #unset \$(git rev-parse --local-env-vars)  # 将 git hook 特定的环境变量值全部置为空
 
     cd $zCodePath &&  # 必须首先切换路径，否则 reset 不会执行
