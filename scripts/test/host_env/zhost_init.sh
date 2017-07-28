485f1cbf6cd7bb35c7b3716ac02a0b0b58b5e1fd
git_shadow <git_shadow@yixia.com>
2017-07-28 20:03:32 +0800

    __

--- scripts/test/host_env/zhost_init.sh
+++ scripts/test/host_env/zhost_init.sh
@@ -2,10 +2,11 @@
 zProjName="miaopai"
 zCodePath=/home/git/$zProjName
 
-zCurDir=`pwd`
+zCurDir=`pwd`  // test/host_env 目录
 
 rm -rf $zCodePath
 mkdir $zCodePath
+mkdir $zCodePath/.git_shadow
 
 cd $zCodePath
 git init .
@@ -23,7 +24,7 @@ git commit --allow-empty -m "_"
 git branch -m master client # 将master分支名称更改为client
 git branch server # 创建server分支
 
-cp ${zCurDir}/test/host_env/zECS_git_post-update.sh ${zCodePath}/.git/hooks/post-update
+cp ${zCurDir}/zECS_git_post-update.sh ${zCodePath}/.git/hooks/post-update
 chmod u+x ${zCodePath}/.git/hooks/post-update
 
 chown -R git:git /home/git
