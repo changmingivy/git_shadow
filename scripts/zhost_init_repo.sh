6f2924a9ee0c960da56f439157bd9f5a97ba0b5e
git_shadow <git_shadow@yixia.com>
2017-08-03 00:44:47 +0800

    脚本：master-slaves模式已跑通

--- scripts/zhost_init_repo.sh
+++ scripts/zhost_init_repo.sh
@@ -2,32 +2,54 @@
 # TEST:PASS
 zPathOnMaster=$1
 zPathOnHost=`echo $1 | grep -oP '(?<=/home/git).*'`
-zHostAddrListPath="${zPathOnMaster}/.git_shadow/info/host_ip_all.txt"
+zMajorHostAddrListPath="${zPathOnMaster}/.git_shadow/info/host_ip_major.txt"
+zAllHostAddrListPath="${zPathOnMaster}/.git_shadow/info/host_ip_all.txt"
 
-for x in `cat $zHostAddrListPath`; do
-    ssh $x "
-        rm -rf $zPathOnHost &&\
-        mkdir -p $zPathOnHost/.git_shadow &&\
+zMajorIpList=`cat $zMajorHostAddrListPath`
+zAllIpList=`cat $zAllHostAddrListPath`
+
+for x in `cat $zMajorHostAddrListPath`; do
+    (\
+        ssh $x "
+            if [[ 0 -ne \`ls -d $zPathOnHost | wc -l\` ]];then exit; fi &&
+            mkdir -p $zPathOnHost/.git_shadow &&
 \
-        cd $zPathOnHost/.git_shadow &&\
-        git init . &&\
-        git config user.name "git_shadow" &&\
-        git config user.email "git_shadow@$x" &&\
-        git commit --allow-empty -m "__init__" &&\
-        git branch -f server &&\
+            cd $zPathOnHost/.git_shadow &&
+            git init . &&
+            git config user.name "git_shadow" &&
+            git config user.email "git_shadow@$x" &&
+            git commit --allow-empty -m "__init__" &&
+            git branch -f server &&
 \
-        cd $zPathOnHost &&\
-        git init . &&\
-        git config user.name "`basename $zPathOnHost`" &&\
-        git config user.email "`basename ${zPathOnHost}`@$x" &&\
-        git commit --allow-empty -m "__init__" &&\
-        git branch -f server
-        "
-
-    scp zhost_post-update.sh git@${x}:${zPathOnHost}/.git/hooks/post-update
-
-    ssh $x "
-        eval sed -i 's%\<PROJ_PATH\>%${zPathOnHost}%g' ${zPathOnHost}/.git/hooks/post-update &&\
-        chmod 0755 ${zPathOnHost}/.git/hooks/post-update
-        "
+            cd $zPathOnHost &&
+            git init . &&
+            git config user.name "`basename $zPathOnHost`" &&
+            git config user.email "`basename ${zPathOnHost}`@$x" &&
+            git commit --allow-empty -m "__init__" &&
+            git branch -f server
+            "
+    
+        scp zhost_post-update.sh git@${x}:${zPathOnHost}/.git/hooks/post-update &&
+        scp -r $zPathOnMaster/.git_shadow/info git@${x}:${zPathOnHost}/.git_shadow/ &&
+        scp zhost_init_repo_slave.sh git@${x}:/tmp/zhost_init_repo_slave.sh &&
+        scp zhost_post-update.sh git@${x}:/tmp/zhost_post-update.sh &&
+    
+        ssh $x "
+            eval sed -i 's%\<PROJ_PATH\>%${zPathOnHost}%g' ${zPathOnHost}/.git/hooks/post-update &&
+            chmod 0755 ${zPathOnHost}/.git/hooks/post-update
+            "
+    
+        ssh $x "
+            for zAddr in \`ifconfig | grep -oP '(\d+\.){3}\d+' | grep -vE '^(169|127|0|255)\.|\.255$'\`;do
+                if [[ 0 -ne \`echo \"${zMajorIpList}\" | grep -c \$zAddr\` ]];then
+                    zEcsAddrList=\`echo \"${zAllIpList}\" | tr '\n' ' '\`
+                    for zEcsAddr in \$zEcsAddrList;do
+                        if [[ \$zAddr == \$zEcsAddr ]];then continue; fi &&
+                        sh /tmp/zhost_init_repo_slave.sh \$zEcsAddr $zPathOnHost
+                    done
+                    break
+                fi
+            done
+            "
+    ) &
 done
---
+++ scripts/zhost_init_repo_slave.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+zSlaveAddr=$1
+zPathOnHost=$2
+
+ssh $zSlaveAddr "
+    if [[ 0 -ne \`ls -d $zPathOnHost 2>/dev/null | wc -l\` ]];then exit; fi &&
+    mkdir -p $zPathOnHost/.git_shadow &&
+\
+    cd $zPathOnHost/.git_shadow &&
+    git init . &&
+    git config user.name "git_shadow" &&
+    git config user.email "git_shadow@$x" &&
+    git commit --allow-empty -m "__init__" &&
+    git branch -f server &&
+\
+    cd $zPathOnHost &&
+    git init . &&
+    git config user.name "`basename $zPathOnHost`" &&
+    git config user.email "`basename ${zPathOnHost}`@$x" &&
+    git commit --allow-empty -m "__init__" &&
+    git branch -f server
+    " &&
+
+scp /tmp/zhost_post-update.sh git@${zSlaveAddr}:${zPathOnHost}/.git/hooks/post-update &&
+
+ssh $zSlaveAddr "
+    eval sed -i 's%\<PROJ_PATH\>%${zPathOnHost}%g' ${zPathOnHost}/.git/hooks/post-update &&
+    chmod 0755 ${zPathOnHost}/.git/hooks/post-update
+    "
