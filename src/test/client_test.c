63f56f5b60b87671f1d7dae5a5706946617019a3
_ <_@yixia.com>
2017-08-07 16:57:34 +0800

    pcre2库改为自行编译

--- src/test/client_test.c
+++ src/test/client_test.c
@@ -271,7 +271,7 @@ zclient(void) {
     //char zStrBuf[] = "{\"OpsId\":11,\"RepoId\":88,\"CommitId\":1,\"FileId\":0,\"HostId\":0,\"CacheId\":1000000000,\"DataType\":0,\"Data\":\"\"}";
 
     // 布署
-    char zStrBuf[] = "{\"OpsId\":12,\"RepoId\":88,\"CommitId\":3,\"FileId\":-1,\"HostId\":0,\"CacheId\":1000000000,\"DataType\":0,\"Data\":\"\"}";
+    //char zStrBuf[] = "{\"OpsId\":12,\"RepoId\":88,\"CommitId\":3,\"FileId\":-1,\"HostId\":0,\"CacheId\":1000000000,\"DataType\":0,\"Data\":\"\"}";
 
     // 撤销
     //char zStrBuf[] = "{\"OpsId\":13,\"RepoId\":88,\"CommitId\":0,\"FileId\":-1,\"HostId\":0,\"CacheId\":1502031376,\"DataType\":1,\"Data\":\"\"}";
