0b12a6f09e14b4f06fa03235758029393c7d4bba
_ <_@yixia.com>
2017-08-09 23:31:38 +0800

    ____

--- src/core/zserv.c
+++ src/core/zserv.c
@@ -562,7 +562,7 @@ zops_route(void *zpSd) {
     }
 
     // 应对初始化配置为空或指定的ID不合法等场景
-    if ((1 != zMetaIf.OpsId) && ((-1 == zGlobMaxRepoId) || (0 > zMetaIf.RepoId) || (NULL == zppGlobRepoIf[zMetaIf.RepoId]))) {
+    if ((1 != zMetaIf.OpsId) && ((zGlobMaxRepoId < zMetaIf.RepoId) || (0 > zMetaIf.RepoId) || (NULL == zppGlobRepoIf[zMetaIf.RepoId]))) {
         zMetaIf.OpsId = -2;  // 此时代表错误码
         zconvert_struct_to_json_str(zpJsonBuf, &zMetaIf);
         zsendto(zSd, &(zpJsonBuf[1]), strlen(zpJsonBuf) - 1, 0, NULL);
