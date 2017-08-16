e8ec9f030b0c142a7a1eb576bb081eece3e11a81
ECS <ECS@aliyun.com>
2017-08-16 20:57:02 +0800

    ____

--- src/utils/thread_pool/zthread_pool.c
+++ src/utils/thread_pool/zthread_pool.c
@@ -5,25 +5,25 @@
 #define zThreadPollSiz 1024
 
 #define zAdd_To_Thread_Pool(zFunc, zParam) do {\
-    pthread_mutex_lock(&(zThreadPollMutexLock_0));\
+    pthread_mutex_lock(&(zThreadPollMutexLock[0]));\
     while (-1 == zJobQueue) {\
-        pthread_cond_wait(&(zThreadPoolCond_0), &(zThreadPollMutexLock_0));\
+        pthread_cond_wait(&(zThreadPoolCond[0]), &(zThreadPollMutexLock[0]));\
     }\
 \
     zThreadPoll[zJobQueue].OpsFunc = zFunc;\
     zThreadPoll[zJobQueue].p_param = zParam;\
     zThreadPoll[zJobQueue].MarkStart = 1;\
 \
-    pthread_mutex_lock(&(zThreadPollMutexLock_1));\
+    pthread_mutex_lock(&(zThreadPollMutexLock[1]));\
     zJobQueue = -1;\
-    pthread_cond_signal(&(zThreadPoolCond_1));\
-    pthread_mutex_unlock(&(zThreadPollMutexLock_1));\
+    pthread_cond_signal(&(zThreadPoolCond[1]));\
+    pthread_mutex_unlock(&(zThreadPollMutexLock[1]));\
 \
-    pthread_mutex_unlock(&(zThreadPollMutexLock_0));\
+    pthread_mutex_unlock(&(zThreadPollMutexLock[0]));\
 \
-    pthread_mutex_lock(&(zThreadPollMutexLock_2));\
-    pthread_mutex_unlock(&(zThreadPollMutexLock_2));\
-    pthread_cond_signal(&(zThreadPoolCond_2));\
+    pthread_mutex_lock(&(zThreadPollMutexLock[2]));\
+    pthread_mutex_unlock(&(zThreadPollMutexLock[2]));\
+    pthread_cond_signal(&(zThreadPoolCond[2]));\
 } while(0)
 
 typedef struct zThreadJobInfo {
@@ -37,17 +37,8 @@ zThreadJobInfo zThreadPoll[zThreadPollSiz];
 _i zIndex[zThreadPollSiz];
 _i zJobQueue = -1;
 
-pthread_mutex_t zThreadPollMutexLock_0 = PTHREAD_MUTEX_INITIALIZER;
-char zTmp0[128];
-pthread_mutex_t zThreadPollMutexLock_1 = PTHREAD_MUTEX_INITIALIZER;
-char zTmp1[128];
-pthread_mutex_t zThreadPollMutexLock_2 = PTHREAD_MUTEX_INITIALIZER;
-char zTmp2[128];
-pthread_cond_t zThreadPoolCond_0 = PTHREAD_COND_INITIALIZER;
-char zTmp3[128];
-pthread_cond_t zThreadPoolCond_1 = PTHREAD_COND_INITIALIZER;
-char zTmp4[128];
-pthread_cond_t zThreadPoolCond_2 = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t zThreadPollMutexLock[3] = {PTHREAD_MUTEX_INITIALIZER};
+pthread_cond_t zThreadPoolCond[3] = {PTHREAD_COND_INITIALIZER};
 
 void *
 zthread_func(void *zpIndex) {
@@ -55,26 +46,26 @@ zthread_func(void *zpIndex) {
 
     _i i = *((_i *)zpIndex);
 zMark:;
-    pthread_mutex_lock(&(zThreadPollMutexLock_1));
+    pthread_mutex_lock(&(zThreadPollMutexLock[1]));
     while (-1 != zJobQueue) {  // -1: no other thread is ahead of me.
-        pthread_cond_wait(&(zThreadPoolCond_1), &(zThreadPollMutexLock_1));
+        pthread_cond_wait(&(zThreadPoolCond[1]), &(zThreadPollMutexLock[1]));
     }
 
-    pthread_mutex_lock(&(zThreadPollMutexLock_0));
+    pthread_mutex_lock(&(zThreadPollMutexLock[0]));
     zJobQueue = i;
-    pthread_cond_signal(&(zThreadPoolCond_0));
-    pthread_mutex_unlock(&(zThreadPollMutexLock_0));
+    pthread_cond_signal(&(zThreadPoolCond[0]));
+    pthread_mutex_unlock(&(zThreadPollMutexLock[0]));
 
-    pthread_mutex_unlock(&(zThreadPollMutexLock_1));
+    pthread_mutex_unlock(&(zThreadPollMutexLock[1]));
 
     // 0: param is not ready, can not start; 1: param ready, can start.
-    pthread_mutex_lock(&(zThreadPollMutexLock_2));
+    pthread_mutex_lock(&(zThreadPollMutexLock[2]));
     while (1 != zThreadPoll[i].MarkStart) {
-        pthread_cond_wait(&zThreadPoolCond_2, &(zThreadPollMutexLock_2));
+        pthread_cond_wait(&zThreadPoolCond[2], &(zThreadPollMutexLock[2]));
     }
 
     zThreadPoll[i].MarkStart = 0;
-    pthread_mutex_unlock(&(zThreadPollMutexLock_2));
+    pthread_mutex_unlock(&(zThreadPollMutexLock[2]));
 
     zThreadPoll[i].OpsFunc(zThreadPoll[i].p_param);
 
