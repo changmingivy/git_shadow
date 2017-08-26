f6758148dfd67a77b2204f5d13743352e6b48db5
_ <_@yixia.com>
2017-08-26 12:08:54 +0800

    获取文件内容差异时，首先检测文件列表级别数据是否已生成

--- src/core/zserv.c
+++ src/core/zserv.c
@@ -182,7 +182,17 @@ zprint_diff_content(zMetaInfo *zpMetaIf, _i zSd) {
     };
 
     zCheck_CacheId();  // 宏内部会解锁
+
     zCheck_CommitId();  // 宏内部会解锁
+    if (NULL == zGet_OneCommitVecWrapIf(zpTopVecWrapIf, zpMetaIf->CommitId)) {
+        zget_file_list(zpMetaIf);
+    } else {
+        /* 检测缓存是否正在生成过程中 */
+        if (0 == zGet_OneCommitVecWrapIf(zpTopVecWrapIf, zpMetaIf->CommitId)->VecSiz) {
+            pthread_rwlock_unlock( &(zppGlobRepoIf[zpMetaIf->RepoId]->RwLock) );
+            return -11;
+        }
+    }
 
     zCheck_FileId();  // 宏内部会解锁
     if (NULL == zGet_OneFileVecWrapIf(zpTopVecWrapIf, zpMetaIf->CommitId, zpMetaIf->FileId)) {
