482e045e4a507dcebd763158dee5bca963f66444
_ <_@yixia.com>
2017-08-30 15:27:14 +0800

    __

--- scripts/zmaster_init_self.sh
+++ scripts/zmaster_init_self.sh
@@ -5,7 +5,7 @@ zShadowPath="${HOME}/zgit_shadow"
 
 cd $zShadowPath
 git stash
-git pull
+#git pull  # 有时候不希望更新到最新代码
 
 eval sed -i 's%__MASTER_ADDR%${zServAddr}%g' ./scripts/post-update
 eval sed -i 's%__MASTER_PORT%${zServPort}%g' ./scripts/post-update
@@ -23,14 +23,17 @@ touch ${zShadowPath}/conf/master.conf
 rm -rf ${zShadowPath}/bin/*
 
 # 编译正则库
-cd ${zShadowPath}/lib/
-rm -rf pcre2*
-wget https://ftp.pcre.org/pub/pcre/pcre2-10.23.tar.gz
-mkdir pcre2
-tar -xf pcre2-10.23.tar.gz
-cd pcre2-10.23
-./configure --prefix=$HOME/zgit_shadow/lib/pcre2
-make -j 9 && make install
+cd ${zShadowPath}/lib/pcre2
+if [[ 0 -ne $? ]]; then
+    cd ${zShadowPath}/lib
+    rm -rf pcre2*
+    wget https://ftp.pcre.org/pub/pcre/pcre2-10.23.tar.gz
+    mkdir pcre2
+    tar -xf pcre2-10.23.tar.gz
+    cd pcre2-10.23
+    ./configure --prefix=$HOME/zgit_shadow/lib/pcre2
+    make -j 9 && make install
+fi
 
 # 编译主程序，静态库文件路径一定要放在源文件之后
 cc -Wall -Wextra -std=c99 -O2 -lpthread \
