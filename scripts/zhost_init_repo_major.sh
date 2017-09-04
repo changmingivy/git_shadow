30e4090e70ce74e39ee7c99d44390340cadaad40
_ <_@yixia.com>
2017-09-04 17:53:46 +0800

    |

--- scripts/zhost_init_repo_major.sh
+++ scripts/zhost_init_repo_major.sh
@@ -2,36 +2,40 @@
 zMajorAddr=$1
 zPathOnHost=$(echo $2 | sed -n 's%/\+%/%p')
 
-zSelfPid=$$  # 获取自身PID
 zTmpFile=`mktemp /tmp/${zSelfPid}.XXXXXXXX`
-echo $zSelfPid > $zTmpFile
+echo "Orig" > $zTmpFile
 
-(sleep 5; if [[ "" != `cat $zTmpFile` ]]; then kill -9 $zSelfPid; fi; rm $zTmpFile) &  # 防止遇到无效IP时长时间卡住
-
-ssh $zMajorAddr "
-    mkdir -p ${zPathOnHost}_SHADOW &&
-    mkdir -p ${zPathOnHost} &&
+(
+    ssh $zMajorAddr "
+        mkdir -p ${zPathOnHost}_SHADOW &&
+        mkdir -p ${zPathOnHost} &&
 \
-    cd ${zPathOnHost}_SHADOW &&
-    git init . &&
-    git config user.name "git_shadow" &&
-    git config user.email "git_shadow@$x" &&
-    git commit --allow-empty -m "__init__" &&
-    git branch -f server &&
+        cd ${zPathOnHost}_SHADOW &&
+        git init . &&
+        git config user.name "git_shadow" &&
+        git config user.email "git_shadow@$x" &&
+        git commit --allow-empty -m "__init__" &&
+        git branch -f server &&
 \
-    cd $zPathOnHost &&
-    git init . &&
-    git config user.name "MajorHost" &&
-    git config user.email "MajorHost@$x" &&
-    git commit --allow-empty -m "__init__" &&
-    git branch -f server
-    "
+        cd $zPathOnHost &&
+        git init . &&
+        git config user.name "MajorHost" &&
+        git config user.email "MajorHost@$x" &&
+        git commit --allow-empty -m "__init__" &&
+        git branch -f server
+        "
 
-# 若 SSH 连接成功，则提示后台监视进程已成功执行，不要再kill，防止误杀其它进程
-# 若失败，则以退出码 255 结束进程
-if [[ 0 -eq $? ]]; then
-    echo "" > $zTmpFile
+        if [[ (0 -eq $?) && ("Orig" == `cat ${zTmpFile}`) ]]; then
+            echo "Success" > $zTmpFile
+        fi
+) &
+
+# 防止遇到无效IP时，长时间阻塞；若失败，则以退出码 255 结束进程
+sleep 5
+if [[ "Success" == `cat ${zTmpFile}` ]]; then
+    rm $zTmpFile
+    exit 0
 else
-    echo "" > $zTmpFile
+    rm $zTmpFile
     exit 255
 fi
