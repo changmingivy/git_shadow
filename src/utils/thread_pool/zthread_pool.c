069f769c98607c70f24cf1c8b75f1555b3f18bc5
范辉 <hui.fan@kissos.org>
2017-09-06 20:00:44 +0800

    ____

--- src/utils/thread_pool/zthread_pool.c
+++ src/utils/thread_pool/zthread_pool.c
@@ -8,7 +8,7 @@ typedef void * (* zThreadPoolOps) (void *);  // 线程池回调函数
 
 struct zOpsObjInfo {
     zThreadPoolOps func;
-    void *p_parm;
+    void *p_param;
     struct zOpsObjInfo *p_next;
 };
 typedef struct zOpsObjInfo zOpsObjInfo;
@@ -28,9 +28,9 @@ pthread_t ____zTidTrash____;
 void *
 zthread_func(void *zpIf) {
     pthread_detach(pthread_self());  // 即使该步出错，也无处理错误，故不必检查返回值
-    zOpsObjInfo *zpOpsObjIf;
     zThreadTaskInfo *zpThreadTaskIf = (zThreadTaskInfo *) zpIf;
 
+    zOpsObjInfo *zpOpsObjIf;
 zMark:
     pthread_mutex_lock(&(zTaskQueueIf[zpThreadTaskIf->Index].MutexLock));
     while (NULL == zTaskQueueIf[zpThreadTaskIf->Index].p_OpsObjIf) {
@@ -40,7 +40,7 @@ zMark:
     zTaskQueueIf[zpThreadTaskIf->Index].p_OpsObjIf = zTaskQueueIf[zpThreadTaskIf->Index].p_OpsObjIf->p_next;
     pthread_mutex_unlock(&(zTaskQueueIf[zpThreadTaskIf->Index].MutexLock));
 
-    zpOpsObjIf->func(zpOpsObjIf->p_parm);
+    zpOpsObjIf->func(zpOpsObjIf->p_param);
 
     free(zpOpsObjIf);  // 任务完成，释放静态参数内存
     goto zMark;
@@ -53,7 +53,7 @@ ztmp_thread_func(void *zpIf) {
     pthread_detach(pthread_self());  // 即使该步出错，也无处理错误，故不必检查返回值
     zOpsObjInfo *zpOpsObjIf = (zOpsObjInfo *) zpIf;
 
-    zpOpsObjIf->func(zpOpsObjIf->p_parm);
+    zpOpsObjIf->func(zpOpsObjIf->p_param);
 
     free(zpOpsObjIf);  // 任务完成，释放静态参数内存
     return NULL;
@@ -81,16 +81,22 @@ zthread_poll_init(void) {
         zOpsObjInfo *zpOpsObjIf;\
         zMem_Alloc(zpOpsObjIf, zOpsObjInfo, 1);\
         zpOpsObjIf->func = zFunc;\
-        zpOpsObjIf->p_parm = zParam;\
+        zpOpsObjIf->p_param = zParam;\
         pthread_create(&____zTidTrash____, NULL, ztmp_thread_func, zpOpsObjIf);\
     } else {\
-        zOpsObjInfo *zpTmpIf;\
-        zpTmpIf = zTaskQueueIf[____zKeepId____].p_OpsObjIf;\
-        if (NULL == zpTmpIf) {\
+        if (NULL == zTaskQueueIf[____zKeepId____].p_OpsObjIf) {\
             zMem_Alloc(zTaskQueueIf[____zKeepId____].p_OpsObjIf, zOpsObjInfo, 1);\
+            zTaskQueueIf[____zKeepId____].p_OpsObjIf->func = zFunc;\
+            zTaskQueueIf[____zKeepId____].p_OpsObjIf->p_param = zParam;\
+            zTaskQueueIf[____zKeepId____].p_OpsObjIf->p_next = NULL;\
         } else {\
+            zOpsObjInfo *zpTmpIf;\
+            zpTmpIf = zTaskQueueIf[____zKeepId____].p_OpsObjIf;\
             while (NULL != zpTmpIf->p_next) { zpTmpIf = zpTmpIf->p_next; }\
             zMem_Alloc(zpTmpIf->p_next, zOpsObjInfo, 1);\
+            zpTmpIf->p_next->func = zFunc;\
+            zpTmpIf->p_next->p_param = zParam;\
+            zpTmpIf->p_next->p_next = NULL;\
         }\
         pthread_mutex_unlock(&(zTaskQueueIf[____zKeepId____].MutexLock));\
         pthread_cond_signal(&(zTaskQueueIf[____zKeepId____].CondVar));\
