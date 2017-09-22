23fb4a93cc591d7ce039c096c0b1afe9f2b0a5e4
fanhui <hui.fan@mail.ru>
2017-09-22 17:16:27 +0800

    post-update

--- tools/zpost-update.sh
+++ tools/zpost-update.sh
@@ -8,46 +8,6 @@ export zPathOnHost="__PROJ_PATH"
 zProjName=`basename ${zPathOnHost}`
 zProjOnLinePath=`dirname \`dirname ${zPathOnHost}\``
 
-# 当前hook执行过程中要去掉执行权限，防止以下的git操作触发hook无限循环
-chmod a-x ${zPathOnHost}/.git/hooks/post-update
-
-# 清除可能存在的由于 git 崩溃残留的锁文件
-rm -f ${zPathOnHost}/.git/index.lock
-rm -f ${zPathOnHost}_SHADOW/.git/index.lock
-
-cd ${zPathOnHost}_SHADOW
-export GIT_DIR="${zPathOnHost}_SHADOW/.git"
-\ls -a | grep -Ev '^(\.|\.\.|\.git)$' | xargs rm -rf
-git stash
-git stash clear
-git pull --force ./.git server:master >/dev/null 2>&1
-git reset -q --hard  #git reset -q --hard `git log -1 server --format=%H`
-if [[ 0 -ne $? ]]; then exit 255; fi
-
-cd $zPathOnHost
-export GIT_DIR="${zPathOnHost}/.git"
-\ls -a | grep -Ev '^(\.|\.\.|\.git)$' | xargs rm -rf
-git stash
-git stash clear
-git pull --force ./.git server:master >/dev/null 2>&1
-git reset -q --hard  #git reset -q --hard `git log -1 server --format=%H`
-
-# 校验布署结果
-zMasterSig=`git log master -1 --format=%H`
-zServerSig=`git log server -1 --format=%H`
-if [[ $zMasterSig != $zServerSig
-    || 0 -ne `git status --short | wc -l`
-    || `cat ${zPathOnHost}_SHADOW/.____dp-SHA1.res` != `find . -path './.git' -prune -o -type f -print | sort | xargs cat | sha1sum | grep -oP '^\S+'` ]]
-then
-    exit 255
-fi
-
-# 'B' 用于标识这是布署状态回复，'A' 用于标识远程主机初始化状态回复
-cd ${zPathOnHost}_SHADOW  # 务必切换路径，回复脚本内用了相对路径
-echo -e "\n\n[`date`]" >> /tmp/.____post-deploy.log 2>&1
-sh -x ./tools/zclient_reply.sh "__MASTER_ADDR" "__MASTER_PORT" "B" "${zMasterSig}" >> /tmp/.____post-deploy.log 2>&1
-
-
 ######################################################################
 # 采取换软链接的方式，避免推送大量代码过程中线上代码出现不一致的情况 #
 ######################################################################
@@ -87,7 +47,7 @@ cd $zPathOnHost
 git commit --allow-empty -m "____Auto Commit By Deploy System____"
 git push --force ./.git master:server >/dev/null 2>&1
 
-# 自我更新
+# 更新 post-update
 rm ${zPathOnHost}/.git/hooks/post-update
 cp ${zPathOnHost}_SHADOW/tools/post-update ${zPathOnHost}/.git/hooks/post-update
 chmod 0755 ${zPathOnHost}/.git/hooks/post-update
