d592ba6243776d309e9f8a501b65643b5433afd0
kt10 <kt10@github.com>
2017-09-24 16:52:00 +0800

    qemu

--- qemu_kvm/start_base_99.sh
+++ qemu_kvm/start_base_99.sh
@@ -19,13 +19,13 @@ iptables -t nat -A POSTROUTING -s 172.16.0.0/16 -o $zHostNatIf -j SNAT --to-sour
 qemu-system-x86_64 \
 -enable-kvm \
 -machine q35,accel=kvm -device intel-iommu \
--cpu host -smp 2,sockets=2,cores=1,threads=1 \
--m 2048 \
+-cpu host -smp 4,sockets=4,cores=1,threads=1 \
+-m 4096 \
 -netdev tap,ifname=BASE,script=tap.sh,downscript=no,vhost=on,id=vmNic_BASE -device virtio-net-pci,mac=00:e0:4c:49:99:99,netdev=vmNic_BASE \
--drive file=/home/images/CentOS_base.img,if=none,cache=writeback,media=disk,id=vmDisk_BASE -device virtio-blk-pci,drive=vmDisk_BASE \
+-drive file=/home/fh/Qemu/IMGs/CentOS_base.img,if=none,cache=writeback,media=disk,id=vmDisk_BASE -device virtio-blk-pci,drive=vmDisk_BASE \
 -boot order=cd \
 -name vmBASE \
 -vnc :99 \
 -daemonize
 
-#-drive file=/home/ISOs/CentOS.iso,readonly=on,media=cdrom \
+#-drive file=/home/fh/Qemu/ISOs/CentOS_6.iso,readonly=on,media=cdrom \
