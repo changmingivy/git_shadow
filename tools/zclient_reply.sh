0200bf5f803990da0feb73073fe8f365dd785e5e
fanhui <hui.fan@mail.ru>
2017-09-26 12:31:46 +0800

    兼容旧的 .____req-deploy.sh

--- tools/zclient_reply.sh
+++ tools/zclient_reply.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 zMasterAddr=$1
 zMasterPort=$2
-zReplyType=$3  # 'B' 用于标识这是布署状态回复，'A' 用于标识远程主机初始化状态回复
-zMasterSig=$4
+zMasterSig=$3
+zReplyType=$4  # 'B' 用于标识这是布署状态回复，'A' 用于标识远程主机初始化状态回复
 
 zRelativeRepoIdPath="./info/repo_id"
 
@@ -20,16 +20,16 @@ do
     # 首先使用 notice 工具回复，之后使用 BASH 回复
     ./tools/notice "$zMasterAddr" "$zMasterPort" "8" "`cat ${zRelativeRepoIdPath}`" "$zIPv4NumAddr" "${zMasterSig}" "${zReplyType}"
 
-#    # 关闭套接字读写端，防止先前已打开相同描述符
-#    #exec 777>&-
-#    #exec 777<&-
-#    # 建立 TCP 连接
-#    exec 777>/dev/tcp/${zMasterAddr}/${zMasterPort}
-#
-#    # 发送正文
-#    printf "[{\"OpsId\":8,\"ProjId\":`cat ${zRelativeRepoIdPath}`,\"HostId\":${zIPv4NumAddr},\"data\":${zMasterSig},\"ExtraData\":${zReplyType}}]">&777
-#
-#    # 关闭读写端
-#    #exec 777<&-
-#    exec 777>&-
+    # 关闭套接字读写端，防止先前已打开相同描述符
+    exec 777>&-
+    exec 777<&-
+    # 建立 TCP 连接
+    exec 777>/dev/tcp/${zMasterAddr}/${zMasterPort}
+
+    # 发送正文
+    printf "[{\"OpsId\":8,\"ProjId\":`cat ${zRelativeRepoIdPath}`,\"HostId\":${zIPv4NumAddr},\"data\":${zMasterSig},\"ExtraData\":${zReplyType}}]">&777
+
+    # 关闭读写端
+    #exec 777<&-
+    exec 777>&-
 done
