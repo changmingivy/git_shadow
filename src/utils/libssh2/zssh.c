5beedca5b16ff5e153b1f2d4bdb43e4adc386d35
fanhui <hui.fan@mail.ru>
2017-09-30 11:58:16 +0800

    libssh2

--- src/utils/libssh2/zssh.c
+++ src/utils/libssh2/zssh.c
@@ -35,8 +35,8 @@ zwait_socket(_i zSd, LIBSSH2_SESSION *zSession) {
  */
 _i
 zssh_exec(char *zpHostIpv4Addr, char *zpHostPort, const char *zpCmd,
-        const char *zpUserName, const char *zpPubKeyPath, const char *zpPrivateKeyPath, const char *zpPassWd,
-        _i zAuthType, char *zpRemoteOutPutBuf, _ui zSiz, pthread_mutex_t *zpCcutLock) {
+        const char *zpUserName, const char *zpPubKeyPath, const char *zpPrivateKeyPath, const char *zpPassWd, _i zAuthType,
+        char *zpRemoteOutPutBuf, _ui zSiz, pthread_mutex_t *zpCcutLock) {
 
     _i zSd, zRet, zErrNo;
     LIBSSH2_SESSION *zSession;
@@ -49,68 +49,96 @@ zssh_exec(char *zpHostIpv4Addr, char *zpHostPort, const char *zpCmd,
         return -1;
     }
 
-    if (0 > (zSd = ztcp_connect(zpHostIpv4Addr, zpHostPort, AI_NUMERICHOST | AI_NUMERICSERV))) { return -1; }
-    if (0 != (zSession = libssh2_session_init())) { return -1; }
+    if (0 != (zSession = libssh2_session_init())) {  // need lock ???
+        libssh2_exit();
+        return -1;
+    }
+
+    if (0 > (zSd = ztcp_connect(zpHostIpv4Addr, zpHostPort, AI_NUMERICHOST | AI_NUMERICSERV))) {
+        libssh2_session_free(zSession);
+        libssh2_exit();
+        return -1;
+    }
 
     /* tell libssh2 we want it all done non-blocking */ 
     libssh2_session_set_blocking(zSession, 0);
  
     while (LIBSSH2_ERROR_EAGAIN == (zRet = libssh2_session_handshake(zSession, zSd)));
-    if (0 != zRet) { return -1; }
+    if (0 != zRet) {
+        libssh2_session_free(zSession);
+        libssh2_exit();
+        return -1;
+    }
  
     if (0 == zAuthType) {  /* authenticate via zpPassWd */ 
         while (LIBSSH2_ERROR_EAGAIN == (zRet = libssh2_userauth_password(zSession, zpUserName, zpPassWd)));
-        if (zRet) { goto shutdown; }
-    } else { /* public key */ 
+    } else {  /* public key */ 
         while (LIBSSH2_ERROR_EAGAIN == (zRet = libssh2_userauth_publickey_fromfile(zSession, zpUserName, zpPubKeyPath, zpPrivateKeyPath, zpPassWd)));
-        if (0 != zRet) { goto shutdown; }
+    }
+    if (0 != zRet) {
+        libssh2_session_free(zSession);
+        libssh2_exit();
+        return -1;
     }
  
     /* Exec non-blocking on the remove host */ 
     while((NULL ==  (zChannel= libssh2_channel_open_session(zSession)))
-			&& (LIBSSH2_ERROR_EAGAIN == libssh2_session_last_error(zSession,NULL,NULL,0))) {
-		zwait_socket(zSd, zSession);
-	}
-    if(NULL == zChannel) { exit(1); }
+            && (LIBSSH2_ERROR_EAGAIN == libssh2_session_last_error(zSession, NULL, NULL,0))) {
+        zwait_socket(zSd, zSession);
+    }
+    if(NULL == zChannel) {
+        libssh2_session_disconnect(zSession, "");
+        libssh2_session_free(zSession);
+        libssh2_exit();
+        return -1;
+    }
 
     while(LIBSSH2_ERROR_EAGAIN == (zRet = libssh2_channel_exec(zChannel, zpCmd))) { zwait_socket(zSd, zSession); }
-    if( 0 != zRet) { exit(1); }
+    if( 0 != zRet) {
+        libssh2_channel_free(zChannel);
+        libssh2_session_disconnect(zSession, "");
+        libssh2_session_free(zSession);
+        libssh2_exit();
+        return -1;
+    }
 
-	if (NULL != zpRemoteOutPutBuf) {
-    	for(;;) {
-    	    _i zRet;
-    	    do {
-    	        if(0 < (zRet = libssh2_channel_read(zChannel, zpRemoteOutPutBuf, zSiz))) {
-    	            zpRemoteOutPutBuf += zRet;
-    	            zSiz -= zRet;
-    	        } else {
-    	            if(LIBSSH2_ERROR_EAGAIN != zRet) { exit(1); }
-    	        }
-    	    } while(0 < zRet);
+    if (NULL != zpRemoteOutPutBuf) {
+        for(;;) {
+            _i zRet;
+            do {
+                if(0 < (zRet = libssh2_channel_read(zChannel, zpRemoteOutPutBuf, zSiz))) {
+                    zpRemoteOutPutBuf += zRet;
+                    zSiz -= zRet;
+                } else {
+                    if(LIBSSH2_ERROR_EAGAIN != zRet) {
+                        libssh2_channel_free(zChannel);
+                        libssh2_session_disconnect(zSession, "");
+                        libssh2_session_free(zSession);
+                        libssh2_exit();
+                        return -1;
+                    }
+                }
+            } while(0 < zRet);
  
-    	    /* this is due to blocking that would occur otherwise so we loop on
-    	       this condition */ 
-    	    if( zRet == LIBSSH2_ERROR_EAGAIN ) { zwait_socket(zSd, zSession); }
-    	    else { break; }
-    	}
-	}
+            if( zRet == LIBSSH2_ERROR_EAGAIN ) { zwait_socket(zSd, zSession); }
+            else { break; }
+        }
+    }
 
     zErrNo = -1;
-	while(LIBSSH2_ERROR_EAGAIN == (zRet = libssh2_channel_close(zChannel))) { zwait_socket(zSd, zSession); } 
+    while(LIBSSH2_ERROR_EAGAIN == (zRet = libssh2_channel_close(zChannel))) { zwait_socket(zSd, zSession); } 
     if(0 == zRet) {
         zErrNo = libssh2_channel_get_exit_status(zChannel);
         libssh2_channel_get_exit_signal(zChannel, &zpExitSingal, NULL, NULL, NULL, NULL, NULL);
     }
- 
-	if (zpExitSingal) { zErrNo = -1; }
+    if (NULL != zpExitSingal) { zErrNo = -1; }
  
     libssh2_channel_free(zChannel);
     zChannel= NULL;
  
-shutdown:
     libssh2_session_disconnect(zSession, "Bye");
     libssh2_session_free(zSession);
- 
+
     close(zSd);
     libssh2_exit();
  
