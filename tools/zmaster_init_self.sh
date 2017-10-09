34c056dc3b30baa779ca2b64bf3ab1803731826b
fanhui <hui.fan@mail.ru>
2017-10-09 09:15:47 +0800

    cc ====> clang

--- tools/zmaster_init_self.sh
+++ tools/zmaster_init_self.sh
@@ -32,7 +32,7 @@ cmake -DCMAKE_INSTALL_PREFIX=${zShadowPath}/lib/libssh2 -DBUILD_SHARED_LIBS=OFF 
 cmake --build . --target install
 
 # 编译主程序，静态库文件路径一定要放在源文件之后
-cc -Wall -Wextra -std=c99 -O2 -lpthread -lssl -lcrypto \
+clang -Wall -Wextra -std=c99 -O2 -lpthread -lssl -lcrypto \
     -I${zShadowPath}/inc \
     -I${zShadowPath}/lib/libssh2/include \
     -o ${zShadowPath}/bin/git_shadow \
@@ -41,7 +41,7 @@ cc -Wall -Wextra -std=c99 -O2 -lpthread -lssl -lcrypto \
 strip ${zShadowPath}/bin/git_shadow
 
 # 编译 zssh_XXXX 程序，用于中转机代替 OpenSSH 更高效并发执行 SSH 连接
-cc -Wall -Wextra -std=c99 -O2 -lpthread -lssl -lcrypto \
+clang -Wall -Wextra -std=c99 -O2 -lpthread -lssl -lcrypto \
     -I${zShadowPath}/inc \
     -I${zShadowPath}/lib/libssh2/include \
     -o ${zShadowPath}/tools/zssh \
@@ -50,7 +50,7 @@ cc -Wall -Wextra -std=c99 -O2 -lpthread -lssl -lcrypto \
 strip ${zShadowPath}/tools/zssh
 
 # 编译 notice 程序，用于通知主程序有新的提交记录诞生
-cc -Wall -Wextra -std=c99 -O2 \
+clang -Wall -Wextra -std=c99 -O2 \
     -I${zShadowPath}/inc \
     -o ${zShadowPath}/tools/notice \
     ${zShadowPath}/src/extra/znotice.c
