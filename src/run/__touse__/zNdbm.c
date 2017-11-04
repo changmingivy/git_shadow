b0e7b20815ed4ebcb09fea930c1d5c2291e50e5b
kt <kt@kissos.org>
2017-11-04 22:30:07 +0800

    DB plan B: ndbm && pgsql

---
+++ src/run/__touse__/zNdbm.c
@@ -0,0 +1,29 @@
+#include <fcntl.h>
+#include <gdbm/ndbm.h>
+
+// DBM *
+// dbm_open(const char *base,	int flags, mode_t mode);
+// 
+// void
+// dbm_close(DBM *db);
+// 
+// int
+// dbm_store(DBM *db,	datum key, datum data, int flags);
+// 
+// datum
+// dbm_fetch(DBM *db,	datum key);
+// 
+// int
+// dbm_delete(DBM *db, datum key);
+// 
+// datum
+// dbm_firstkey(DBM *db);
+// 
+// datum
+// dbm_nextkey(DBM *db);
+// 
+// int
+// dbm_error(DBM *db);
+// 
+// int
+// dbm_clearerr(DBM *db);
