45e7477a6739c081283f20f9e109c456d600cfd7
kt <kt@kissos.org>
2017-11-26 00:01:14 +0800

    FIX prev

--- tools/____req-deploy.sh
+++ tools/____req-deploy.sh
@@ -4,7 +4,7 @@ zIpV4List=`ip addr | grep -oP '(?<=inet\s)(\w|\.|:)+(?=(/|\s))' | grep -vE '^(12
 zIpV6List=`ip addr | grep -oP '(?<=inet6\s)(\w|\.|:)+(?=(/|\s))' | grep -viE '(^::1$)|(^fe80::)'`
 
 
-for zProjMetaPath in `find /home/git/.____DpSystem -maxdepth 2 -type d | grep -E '^.+_SHADOW$'`
+for zProjMetaPath in `find ${HOME}/.____DpSystem -maxdepth 2 -type d | grep -E '^.+_SHADOW$'`
 do
     cd $zProjMetaPath && bash ./tools/zhost_self_deploy.sh "${zIpV4List} ${zIpV6List}" &
 done
