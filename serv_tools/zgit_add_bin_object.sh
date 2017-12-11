2ba1536ee1adeb4e2c8fa20893ec0b9b712875c3
test <test@test>
2017-12-11 14:54:36 +0800

    历史记录清理脚本更新

--- serv_tools/zgit_add_bin_object.sh
+++ serv_tools/zgit_add_bin_object.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+
+# which branch to clean...
+zBranchName=$1
 
 git stash
 git stash clear
@@ -24,13 +27,13 @@ find . -type d | xargs ls -gGA --time-style=long-iso > ____version____
 git add --all .
 git commit -m "_"
 
-# 若 .git 占用空间超过 200M，则执行清理，仅保留最近的 10 次提交
-if [[ 200 -lt `du -sm .git | grep -o '[0-9]\+'` ]]; then
+# 若 .git 占用空间超过 50M，则执行清理，仅保留最近的 10 次提交
+if [[ 50 -lt `du -sm .git | grep -o '[0-9]\+'` ]]; then
     zBaseRef=`git log --format=%H | head -10 | tail -1`
 
     git checkout --orphan temp ${zBaseRef}
     git commit -m "clean old refs"
-    git rebase --onto temp ${zBaseRef} master
+    git rebase --onto temp ${zBaseRef} ${zBranchName}
     git branch -D temp
 
     git reflog expire --expire=now --all
