970cdf2e844ead3ba312981f10cdd4d46623e9c7
test <test@test>
2017-12-22 11:59:57 +0800

    test.sh 字段名称同步更新

--- src/extra/test.sh
+++ src/extra/test.sh
@@ -1,31 +1,28 @@
 #/bin/sh
 
-# 列出单个项目元信息
-#zContent="{\"OpsId\":6,\"ProjId\":11}"
-
 # 创建新项目
-#zContent="{\"OpsId\":1,\"ProjId\":\"111\",\"PathOnHost\":\"/home/git/111_Y\",\"SourceUrl\":\"https://git.coding.net/kt10/FreeBSD.git\",\"SourceBranch\":\"master\",\"SourceVcsType\":\"git\",\"NeedPull\":\"Y\",\"SSHUserName\":\"git\",\"SSHPort\":\"22\"}"
+# zContent="{\"opsId\":1,\"projId\":\"11\",\"pathOnHost\":\"/home/git/11_Y\",\"sourceUrl\":\"https://git.coding.net/kt10/zgit_shadow.git\",\"sourceBranch\":\"master\",\"sourceVcsType\":\"git\",\"needPull\":\"Y\",\"sshUserName\":\"git\",\"sshPort\":\"22\"}"
 
 # 查询版本号列表
-zContent="{\"OpsId\":9,\"ProjId\":11,\"DataType\":0}"
+zContent="{\"opsId\":9,\"projId\":11,\"dataType\":0}"
 
 # 打印差异文件列表
-#zContent="{\"OpsId\":10,\"ProjId\":11,\"RevId\":1,\"CacheId\":1000000000,\"DataType\":0}"
+# zContent="{\"opsId\":10,\"projId\":11,\"revId\":1,\"cacheId\":1000000000,\"dataType\":0}"
 
 # 打印差异文件内容
-#zContent="{\"OpsId\":11,\"ProjId\":11,\"RevId\":0,\"FileId\":0,\"CacheId\":1000000000,\"DataType\":0}"
+#zContent="{\"opsId\":11,\"projId\":11,\"revId\":0,\"fileId\":0,\"cacheId\":1000000000,\"dataType\":0}"
 
 # 布署/撤销
-# zContent="{\"OpsId\":12,\"ProjId\":11,\"RevId\":${1},\"CacheId\":0,\"DataType\":0,\"IpList\":\"::1\",\"IpCnt\":1,\"SSHUserName\":\"git\",\"SSHPort\":\"22\"}"
+# zContent="{\"opsId\":12,\"projId\":11,\"revId\":${1},\"cacheId\":0,\"dataType\":0,\"ipList\":\"::1\",\"ipCnt\":1,\"sshUserName\":\"git\",\"sshPort\":\"22\"}"
 
 # 实时进度查询
-# zContent="{\"OpsId\":15,\"ProjId\":11}"
+# zContent="{\"opsId\":15,\"projId\":11}"
 
-# 系统升级更新
-# zContent="{\"OpsId\":2}"
+# 布署系统升级后，目标机组件更新
+# zContent="{\"opsId\":2}"
 
-# 更换源库分支
-# zContent="{\"OpsId\":3,\"ProjId\":11,\"CodeSyncBranch\":\"master2\"}"
+# 更换源库 URL 或分支
+# zContent="{\"opsId\":3,\"projId\":11,\"sourceURL\":\"https://githuhub.com/kt10/zgit_shadow.git\",\"sourceBranch\":\"master2\"}"
 
 zTcpSend() {
     exec 44<>/dev/tcp/${1}/${2}
