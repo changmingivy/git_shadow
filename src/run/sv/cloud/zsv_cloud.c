6ef482732b156f89559736e07f3fa6bea87d07d0
test <test@test>
2018-01-27 23:56:36 +0800

    ecs 数据提取逻辑已清晰

--- src/run/sv/cloud/zsv_cloud.c
+++ src/run/sv/cloud/zsv_cloud.c
@@ -19,48 +19,159 @@ struct zSVCloud__ zSVCloud_ = {
     .data_sync = zcloud_data_sync,
 };
 
+struct zEcsDisk__ {
+    char *p_name;
+    struct zEcsDisk__ *p_next;
+};
+
+struct zEcsNetIf__ {
+    char *p_name;
+    struct zEcsNetIf__ *p_next;
+};
+
+// TODO 开辟专用的内存池，大小 4M，批量取完数据后，一次性释放
+#define zHASH_KEY_SIZ (1 + 23 / sizeof(_ull))
+struct zEcsData__ {
+    struct zEcsData__ *p_next;
+
+    /*
+     * 以链表形式集齐实例所有 device 的名称，
+     * 用于查询对应设备的监控数据
+     */
+    struct zEcsDisk__ disk;
+    struct zEcsNetIf__ netIf;
+
+    /* HASH KEY */
+    union {
+        _ull hashKey[zHASH_KEY_SIZ];
+        char id[23];  // instanceID(22 char) + '\0'
+    };
+
+    /* 可直接取到的不需要额外加工的数据项 */
+    _i timeStamp;
+    _s cpu;
+    _s mem;
+    _s load[3];
+
+    /* 分别处于 tcp 的 11 种状态的连接计数 */
+    _us tcpState[11];
+
+    /*
+     * 取磁盘列表的时，可得到以 GB 为单位的容量，
+     * 不需要从监控数据中再取一次，
+     * 所有磁盘容量加和之后，换算成 M，保持与 diskSpent 的单位统一
+     */
+    _i diskTotal;
+
+    /* 并发提取如下九项数据时，需要的多线程游标 */
+    void *p_diskCursor[5];  // 磁盘相关的 5 项
+    void *p_netIfCursor[4];  // 网卡相关的 4 项
+
+    /*
+     * 首先将 byte 换算成 M(/1024/1024)，以 M 为单位累加计数，
+     * 集齐所有磁盘的数据后，计算实例的全局磁盘总使用率
+     */
+    _i diskSpent;
+
+    /*
+     * 首先将 byte 换算成 KB(/1024)，以 KB 为单位累加计数，
+     * 集齐所有设备的数据后，取得最终的 sum 值
+     */
+    _i disk_rdkb;
+    _i disk_wrkb;
+
+    _i disk_rdiops;
+    _i disk_wriops;
+
+    _i net_rdkb;
+    _i net_wrkb;
+
+    _i net_rdiops;
+    _i net_wriops;
+};
+
 /* 云监控模块 DB 预建 */
 static void
 zsv_cloud_prepare(void) {
-//    _i zErrNo = 0;
-//
-//    zErrNo = zPgSQL_.exec_once(zRun_.p_sysInfo_->pgConnInfo,
-//            "CREATE TABLE IF NOT EXISTS supervisor_log"
-//            "("
-//            "repo_id         smallint DEFAULT 0,"  /* 所属项目 ID */
-//            "ip              inet NOT NULL,"
-//            "time_stamp      int NOT NULL,"
-//            "loadavg5        int NOT NULL,"  /* system load average recent 5 mins */
-//            "cpu_r           smallint NOT NULL,"  /* = 10000 * cpu usage rate */
-//            "mem_r           smallint NOT NULL,"  /* = 10000 * mem usage rate */
-//            "disk_io_rd_s    int NOT NULL,"  /* io_read tps spent */
-//            "disk_io_wr_s    int NOT NULL,"  /* io_write tps spent */
-//            "net_io_rd_s     int NOT NULL,"  /* net_read tps spent */
-//            "net_io_wr_s     int NOT NULL,"  /* net_wirte tps spent */
-//            "disk_mu         smallint NOT NULL"  /* = 100 * disk max usage rate: 每次只提取磁盘使用率最高的一个磁盘或分区的使用率，整数格式 0-100，代表 0% - 100% */
-//            ") PARTITION BY RANGE (time_stamp);",
-//            NULL);
-//
-//    if (0 != zErrNo) {
-//        zPRINT_ERR_EASY("");
-//        exit(1);
-//    }
-//
-//    /* 每次启动时尝试创建必要的表，按小时分区（1天 == 3600秒） */
-//    /* 预建 10 天的分区表，没有指明 repo_id 监控数据，一律存放于 0 号表 */
-//    char zSQLBuf[512];
-//    _i zBaseID = time(NULL) / 3600;
-//    for (_i zID = 0; zID < 10 * 24; zID++) {
-//        sprintf(zSQLBuf,
-//                "CREATE TABLE IF NOT EXISTS supervisor_log_%d "
-//                "PARTITION OF supervisor_log FOR VALUES FROM (%d) TO (%d) "
-//                "PARTITION BY LIST (repo_id);",
-//                zBaseID + zID,
-//                3600 * (zBaseID + zID),
-//                3600 * (zBaseID + zID + 1));
-//
-//        zPgSQL_.write_db(zSQLBuf, 0, NULL, 0);
-//    }
+    _i zErrNo = 0;
+
+    zErrNo = zPgSQL_.exec_once(zRun_.p_sysInfo_->pgConnInfo,
+            "CREATE TABLE IF NOT EXISTS sv_ecs_aliyun"
+            "("
+            /*
+             * aliyun 原始值为毫秒，
+             * 存储除以 15000 后的值，即：UNIX 计元至今有多少个 15 秒，
+             * 保障各种监控指标的时间戳均能一一对应
+             */
+            "time_stamp      int NOT NULL,"
+
+            /* 实例 ID，由 22 位字符组成 */
+            "instance_id     char(22) NOT NULL,"
+
+            /*
+             * = 10 * cpu usage rate，阿里云返回的是去掉 % 的值，如：81.66，
+             * 此处乘以 10，以整数形式保留一位小数的精度，如示例便是 817，
+             * 内存使用率计算方式同上
+             */
+            "cpu_rate        smallint NOT NULL,"
+            "mem_rate        smallint NOT NULL,"
+
+            /*
+             * 阿里云返回的值与 top 命令显示的格式一致，如：3.88，
+             * 乘以 1000，之后除以 CPU 核心数，以整数整式保留一位小数的精度，
+             * 不能乘以 10000，可以会导致 smallint 溢出。
+             * = 1000 * system load average recent 1 mins/ 5 mins / 15mins
+             */
+            "load_1m            smallint NOT NULL,"
+            "load_5m            smallint NOT NULL,"
+            "load_15m           smallint NOT NULL,"
+
+            /* 分别处于 tcp 的 11 种状态的连接计数 */
+            "tcp_state_cnt          int[11] NOT NULL,"
+
+            /*
+             * 同一主机可能拥有多个网卡、磁盘，
+             * 仅保留所有设备之和，不存储名细
+             * 磁盘与网络流量类指标，原始单位为 bytes，保留除以 1024 后的值，单位：KB
+             */
+            "disk_rdkb       int NOT NULL,"  /* io_read kbytes/KB */
+            "disk_wrkb       int NOT NULL,"  /* io_write kbytes/KB */
+            "disk_rdiops     int NOT NULL,"  /* io_read tps */
+            "disk_wriops     int NOT NULL,"  /* io_write tps */
+            "net_rdkb        int NOT NULL,"  /* io_read kbytes/KB */
+            "net_wrkb        int NOT NULL,"  /* io_write kbytes/KB */
+            "net_rdiops      int NOT NULL,"  /* io_read tps */
+            "net_wriops      int NOT NULL,"  /* io_write tps */
+
+            /*
+             * 磁盘使用率：
+             * 此项指标需要取回所有磁盘的已使用空间绝对大小与总空间大小，
+             * 之后计算得出最终结果（以百分值的 1000 倍存储），不能直接取阿里云的比率
+             */
+            "disk_rate       smallint NOT NULL"
+            ") PARTITION BY RANGE (time_stamp);",
+            NULL);
+
+    if (0 != zErrNo) {
+        zPRINT_ERR_EASY("");
+        exit(1);
+    }
+
+    /* 每次启动时尝试创建必要的表，按小时分区（1天 == 3600秒） */
+    /* 预建 10 天的分区表，没有指明 repo_id 监控数据，一律存放于 0 号表 */
+    char zSQLBuf[512];
+    _i zBaseID = time(NULL) / 3600;
+    for (_i zID = 0; zID < 10 * 24; zID++) {
+        sprintf(zSQLBuf,
+                "CREATE TABLE IF NOT EXISTS supervisor_log_%d "
+                "PARTITION OF supervisor_log FOR VALUES FROM (%d) TO (%d) "
+                "PARTITION BY LIST (repo_id);",
+                zBaseID + zID,
+                3600 * (zBaseID + zID),
+                3600 * (zBaseID + zID + 1));
+
+        zPgSQL_.write_db(zSQLBuf, 0, NULL, 0);
+    }
 }
 
 /*
