9265e14149b6d38a0fb503b73c3625c02da774b6
test <test@test>
2018-01-31 11:43:16 +0800

    fix repv

--- src/run/sv/cloud/zsv_cloud.c
+++ src/run/sv/cloud/zsv_cloud.c
@@ -754,9 +754,10 @@ zget_sv_net_wriops(void *zp) {
     return NULL;
 }
 
-static void
-zget_sv_base(void) {
+static void *
+zget_sv_base(char * const zpRegion) {
     pthread_t zTid[5];
+    _i i;
 
     void * (* zBaseFunc[5]) (void *) = {
         zget_sv_cpu_rate,
@@ -766,21 +767,24 @@ zget_sv_base(void) {
         zget_sv_load15m,
     };
 
-    for (_i i = 0; i < 5; i++) {
-        if (0 != pthread_create(zTid + i, NULL, zBaseFunc[i], NULL)) {
+    for (i = 0; i < 5; i++) {
+        if (0 != pthread_create(zTid + i, NULL, zBaseFunc[i], zpRegion)) {
             zPRINT_ERR_EASY("pthread_create() err!");
             exit(1);
         }
     }
 
-    for (_i i = 0; i < 5; i++) {
+    for (i = 0; i < 5; i++) {
         pthread_join(zTid[i], NULL);
     }
+
+    return NULL;
 }
 
-static void
-zget_sv_disk(void) {
+static void *
+zget_sv_disk(char * const zpRegion) {
     pthread_t zTid[6];
+    _i i;
 
     void * (* zDiskFunc[6]) (void *) = {
         zget_sv_disk_total,
@@ -791,21 +795,24 @@ zget_sv_disk(void) {
         zget_sv_disk_wriops,
     };
 
-    for (_i i = 0; i < 6; i++) {
-        if (0 != pthread_create(zTid + i, NULL, zDiskFunc[i], NULL)) {
+    for (i = 0; i < 6; i++) {
+        if (0 != pthread_create(zTid + i, NULL, zDiskFunc[i], zpRegion)) {
             zPRINT_ERR_EASY("pthread_create() err!");
             exit(1);
         }
     }
 
-    for (_i i = 0; i < 6; i++) {
+    for (i = 0; i < 6; i++) {
         pthread_join(zTid[i], NULL);
     }
+
+    return NULL;
 }
 
-static void
-zget_sv_net(void) {
+static void *
+zget_sv_net(char * const zpRegion) {
     pthread_t zTid[4];
+    _i i;
 
     void * (* zNetFunc[4]) (void *) = {
         zget_sv_net_rdkb,
@@ -814,21 +821,24 @@ zget_sv_net(void) {
         zget_sv_net_wriops,
     };
 
-    for (_i i = 0; i < 4; i++) {
-        if (0 != pthread_create(zTid + i, NULL, zNetFunc[i], NULL)) {
+    for (i = 0; i < 4; i++) {
+        if (0 != pthread_create(zTid + i, NULL, zNetFunc[i], zpRegion)) {
             zPRINT_ERR_EASY("pthread_create() err!");
             exit(1);
         }
     }
 
-    for (_i i = 0; i < 4; i++) {
+    for (i = 0; i < 4; i++) {
         pthread_join(zTid[i], NULL);
     }
+
+    return NULL;
 }
 
-static void
-zget_sv_tcp_state(void) {
+static void *
+zget_sv_tcp_state(char * const zpRegion) {
     pthread_t zTid[11];
+    _i i;
 
     void * (* zTcpStateFunc[11]) (void *) = {
         zget_sv_tcp_state_LISTEN,
@@ -844,24 +854,38 @@ zget_sv_tcp_state(void) {
         zget_sv_tcp_state_CLOSED,
     };
 
-    for (_i i = 0; i < 11; i++) {
-        if (0 != pthread_create(zTid + i, NULL, zTcpStateFunc[i], NULL)) {
+    for (i = 0; i < 11; i++) {
+        if (0 != pthread_create(zTid + i, NULL, zTcpStateFunc[i], zpRegion)) {
             zPRINT_ERR_EASY("pthread_create() err!");
             exit(1);
         }
     }
 
-    for (_i i = 0; i < 11; i++) {
+    for (i = 0; i < 11; i++) {
         pthread_join(zTid[i], NULL);
     }
+
+    return NULL;
 }
 
 static void
 zget_sv(void) {
-    zget_sv_base();
-    zget_sv_disk();
-    zget_sv_net();
-    zget_sv_tcp_state();
+    pthread_t zTid[sizeof(zpRegion) / sizeof(void *)][4];
+    _i i, j;
+
+    /* 提取所有实例 ID */
+    for (i = 0; i < (_i) (sizeof(zpRegion) / sizeof(void *)); ++i) {
+        pthread_create((zTid + i)[0], NULL, zget_sv_base, zpRegion[i]);
+        pthread_create((zTid + i)[1], NULL, zget_sv_disk, zpRegion[i]);
+        pthread_create((zTid + i)[2], NULL, zget_sv_net, zpRegion[i]);
+        pthread_create((zTid + i)[3], NULL, zget_sv_tcp_state, zpRegion[i]);
+    }
+
+    for (i = 0; i < (_i) (sizeof(zpRegion) / sizeof(void *)); ++i) {
+        for (j = 0; j < 4; j++) {
+            pthread_join(zTid[i][j], NULL);
+        }
+    }
 }
 
 static void
